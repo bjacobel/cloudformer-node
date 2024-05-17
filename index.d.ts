906b395f057db9a10a63b56136a20d9eb48188cb
Brian Jacobel <bjacobel@gmail.com>
2024-05-16 22:10:28 -0400

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -2,14 +2,24 @@ import { CreateStackInput, UpdateStackInput } from 'aws-sdk/clients/cloudformati
 
 type Callback = (error: Error | string, success: boolean) => void
 
+type Params = {
+  Parameters: Record<string, string>
+}
+type Tags = {
+  Tags: Record<string, string>
+}
+
+type CloudformerCreateInput = Partial<Omit<CreateStackInput, 'Parameters', 'Tags'>> & Params & Tags;
+type CloudformerUpdateInput = Partial<Omit<UpdateStackInput, 'Parameters', 'Tags'>> & Params & Tags;
+
 export default class Stack {
   constructor(stackName);
 
-  apply(template: string, options: Partial<CreateStackInput> | Partial<UpdateStackInput>, callback: Callback): void ;
+  apply(template: string, options: CloudformerCreateInput | CloudformerUpdateInput, callback: Callback): void ;
   delete(callback: Callback): void
   outputs(callback: Callback): void
-  update(template: string, options: Partial<UpdateStackInput>, callback: Callback): void
-  create(template: string, options: Partial<CreateStackInput>, callback: Callback): void
+  update(template: string, options: CloudformerUpdateInput, callback: Callback): void
+  create(template: string, options: CloudformerCreateInput, callback: Callback): void
   waitUntilEnd(callback: Callback): void
   isDeployed(callback: Callback): void
   isValidTemplate(template: string, callback: Callback)

41e450781a8c94f6e0fc61c391dd032dbfc0d9aa
Brian Jacobel <bjacobel@gmail.com>
2024-05-16 21:44:00 -0400

    Add types

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module '@bjacobel/cloudformer-node' {
+  import { CreateStackInput, UpdateStackInput } from 'aws-sdk/clients/cloudformation';
+
+  type Callback = (error: Error | string, success: boolean) => void
+
+  export class Stack {
+    constructor(stackName);
+
+    apply(template: string, options: Partial<CreateStackInput> | Partial<UpdateStackInput>, callback: Callback): void ;
+    delete(callback: Callback): void
+    outputs(callback: Callback): void
+    update(template: string, options: Partial<UpdateStackInput>, callback: Callback): void
+    create(template: string, options: Partial<CreateStackInput>, callback: Callback): void
+    waitUntilEnd(callback: Callback): void
+    isDeployed(callback: Callback): void
+    isValidTemplate(template: string, callback: Callback)
+    isDeploySuccessful(callback: Callback): void
+  }
+}
